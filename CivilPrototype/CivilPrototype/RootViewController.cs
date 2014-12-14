63ebe8c5187cb379fca65f53716cedc173f359fe
Carter Henderson <carterh062@gmail.com>
2014-12-14 16:19:44 -0500

    added profile view/edit ui

--- CivilPrototype/CivilPrototype/RootViewController.cs
+++ CivilPrototype/CivilPrototype/RootViewController.cs
@@ -5,6 +5,7 @@ using System.CodeDom.Compiler;
 using FlyoutNavigation;
 using MonoTouch.Dialog;
 using System.Drawing;
+using System.Threading;
 
 namespace CivilPrototype
 {
@@ -12,6 +13,10 @@ namespace CivilPrototype
 	{
 		FlyoutNavigationController navigation;
 		bool userLoggedIn = false;
+		EditProfileButton editButton;
+		UITextView profileView;
+		UIView mainProfileView;
+		UIScrollView scrollProfileView;
 		public RootViewController (IntPtr handle) : base (handle)
 		{
 		}
@@ -22,21 +27,95 @@ namespace CivilPrototype
 			await DataLayer.LoginUser(username,password);
 			this.ViewDidLoad ();
 		}
+
+		private void SlowMethod ()
+		{
+			Thread.Sleep (300);
+			InvokeOnMainThread (delegate {
+				editButton.BackgroundColor = UIColor.White;
+			});
+		}
 		public override void ViewDidLoad ()
 		{
 			base.ViewDidLoad ();
 			userLoggedIn = NSUserDefaults.StandardUserDefaults.BoolForKey ("userLoggedIn");
 			NavigationController.NavigationBarHidden = true;
-
 			if (userLoggedIn) {
 
 				string userFirstName = NSUserDefaults.StandardUserDefaults.StringForKey ("currentUserFirstName");
 				string userLastName = NSUserDefaults.StandardUserDefaults.StringForKey ("currentUserLastName");
 				string userEmail = NSUserDefaults.StandardUserDefaults.StringForKey ("currentUserEmail");
 				string userName = NSUserDefaults.StandardUserDefaults.StringForKey ("currentUserUsername");
-				var editButton = new EditProfileButton (View.Bounds.Width);
+				editButton = new EditProfileButton (View.Bounds.Width);
 				editButton.ButtonTapped += delegate(NSSet touches, UIEvent evt) {
-					editButton.BackgroundColor = UIColor.Blue;
+					editButton.BackgroundColor = DesignConstants.lgrey;
+					ThreadPool.QueueUserWorkItem (o => SlowMethod ());
+					InvokeOnMainThread(delegate{
+						scrollProfileView.Add(new UITextField{
+							Placeholder = "Username",
+							Frame = new RectangleF((View.Bounds.Width/2) - 100,200,200,40),
+							BackgroundColor = UIColor.White,
+							BorderStyle = DesignConstants.TextFieldBorderStyle,
+						});
+						scrollProfileView.Add(new UITextField{
+								Placeholder = "Email",
+								Frame = new RectangleF((View.Bounds.Width/2) - 100,300,200,40),
+								BackgroundColor = UIColor.White,
+								BorderStyle = DesignConstants.TextFieldBorderStyle,
+						});
+						scrollProfileView.Add(new UITextField{
+								Placeholder = "First Name",
+								Frame = new RectangleF((View.Bounds.Width/2) - 100,400,200,40),
+								BackgroundColor = UIColor.White,
+								BorderStyle = DesignConstants.TextFieldBorderStyle,
+							});
+						scrollProfileView.Add(new UITextField{
+								Placeholder = "Last Name",
+								Frame = new RectangleF((View.Bounds.Width/2) - 100,500,200,40),
+								BackgroundColor = UIColor.White,
+								BorderStyle = DesignConstants.TextFieldBorderStyle,
+							}		);
+					
+					});
+
+				};
+				profileView = new UITextView {
+					Text = "Profile",
+					Font = UIFont.FromName (DesignConstants.HeaderFontStyle, DesignConstants.HeaderFontSize),
+					BackgroundColor = DesignConstants.HeaderBackground,
+					TextAlignment = DesignConstants.HeaderAlignment,
+					Frame = new RectangleF (DesignConstants.HeaderFrameX, 
+						DesignConstants.HeaderFrameY, 
+						View.Bounds.Width + DesignConstants.HeaderFrameWidth, 
+						DesignConstants.HeaderFrameHeight)
+				};
+				scrollProfileView = new UIScrollView (new RectangleF (0, 50, View.Bounds.Width, 500)) {
+					new UITextView {
+						Text = "Username: " + NSUserDefaults.StandardUserDefaults.StringForKey("currentUserUsername"),
+						Frame = new RectangleF ((View.Bounds.Width / 2) - 100, 150, 200, 40),
+						BackgroundColor = UIColor.Clear,
+					},
+					new UITextView {
+						Text = "Email: " + NSUserDefaults.StandardUserDefaults.StringForKey("currentUserEmail"),
+						Frame = new RectangleF ((View.Bounds.Width / 2) - 100, 250, 200, 40),
+						BackgroundColor = UIColor.Clear,
+					},
+					new UITextView {
+						Text = "First Name: " + NSUserDefaults.StandardUserDefaults.StringForKey("currentUserFirstName"),
+						Frame = new RectangleF ((View.Bounds.Width / 2) - 100, 350, 200, 40),
+						BackgroundColor = UIColor.Clear,
+					},
+					new UITextView {
+						Text = "Last Name: " + NSUserDefaults.StandardUserDefaults.StringForKey("currentUserLastName"),
+						Frame = new RectangleF ((View.Bounds.Width / 2) - 100, 450, 200, 40),
+						BackgroundColor = UIColor.Clear,
+					},
+				};
+				scrollProfileView.ContentSize = new SizeF (View.Bounds.Width, 650);
+				mainProfileView = new UIView (new RectangleF(0,0,View.Bounds.Width,View.Frame.Height)) {
+					scrollProfileView,
+					profileView,
+					editButton,
 				};
 				var viewsControl = new [] {
 					new NavigableViewController () { Navigation = navigation, 
@@ -55,20 +134,8 @@ namespace CivilPrototype
 						} 
 					},
 					new NavigableViewController () { Navigation = navigation, 
-						MainView = new UIView(View.Bounds){
-							new UITextView
-							{
-								Text = "Profile",
-								Font = UIFont.FromName(DesignConstants.HeaderFontStyle,DesignConstants.HeaderFontSize),
-								BackgroundColor = DesignConstants.HeaderBackground,
-								TextAlignment = DesignConstants.HeaderAlignment,
-								Frame = new RectangleF(DesignConstants.HeaderFrameX, 
-									DesignConstants.HeaderFrameY, 
-									View.Bounds.Width + DesignConstants.HeaderFrameWidth, 
-									DesignConstants.HeaderFrameHeight)
-							},
-							editButton,
-						}  },
+						MainView = mainProfileView,
+					},
 					new NavigableViewController () { Navigation = navigation, 
 						MainView = new UIView(View.Bounds){
 							new UITextView
